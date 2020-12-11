15cd1f1e0129b3131f7ae6bed568fe2177d6e528
yateen <ypdowork@gmail.com>
2020-12-11 12:47:19 +0530

    bootstrp ulpoad

---
+++ application/controllers/Auth.php
@@ -0,0 +1,13 @@
+<?php
+    class Auth extends CI_Controller {
+/*
+This Function will show register page
+*/
+        public function register(){
+            echo "Registration Page";
+        }
+    }
+
+
+
+?>
